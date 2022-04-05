aff320cb3685f9db5ca9920918e6be52f556de30
Ramil Girfanov <ramil.girfanov@gmail.com>
2022-04-05 16:34:01 +0900

    Удалил сториборд. Проверил работоспособность window

---
+++ Navigation/Navigation/MainTabBarControllerViewController.swift
@@ -0,0 +1,29 @@
+//
+//  MainTabBarControllerViewController.swift
+//  Navigation
+//
+//  Created by Рамиль Гирфанов on 05.04.2022.
+//
+
+import UIKit
+
+class MainTabBarControllerViewController: UITabBarController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
