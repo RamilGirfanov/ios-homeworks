281aed0749763668eeaca5c3525d8c473d808313
Ramil Girfanov <ramil.girfanov@gmail.com>
2022-04-17 16:44:52 +0900

    Первый пункт ДЗ готов

--- Navigation/Navigation/Profile/ProfileHeaderView.swift
+++ Navigation/Navigation/Profile/ProfileHeaderView.swift
@@ -9,6 +9,8 @@ import UIKit
 
 class ProfileHeaderView: UIView {
     
+    // MARK: - create UI objects
+    
     let avatarImageView: UIImageView = {
         let avatarImageView = UIImageView()
         
@@ -44,19 +46,50 @@ class ProfileHeaderView: UIView {
         return statusLabel
     }()
     
+    let statusTextField: UITextField = {
+        let statusTextField = UITextField()
+        
+        statusTextField.text = " Set status"
+        statusTextField.textColor = .black
+        statusTextField.font = .systemFont(ofSize: 15, weight: .regular)
+        statusTextField.translatesAutoresizingMaskIntoConstraints = false
+        
+        statusTextField.layer.borderWidth = 1
+        statusTextField.layer.borderColor = UIColor.black.cgColor
+        statusTextField.layer.cornerRadius = 12
+        statusTextField.layer.backgroundColor = UIColor.white.cgColor
+        
+        return statusTextField
+    }()
     
+    let stackView: UIStackView = {
+        let stackView = UIStackView()
+        stackView.axis = .vertical
+        stackView.spacing = 10
+        stackView.distribution = .fillEqually
+        stackView.translatesAutoresizingMaskIntoConstraints = false
+        
+        return stackView
+    }()
     
+    let setStatusButton: UIButton = {
+        let setStatusButton = UIButton()
+        
+        setStatusButton.setTitle("Set status", for: .normal)
+        setStatusButton.setTitleColor(.white, for: .normal)
+        setStatusButton.layer.cornerRadius = 12
+        setStatusButton.backgroundColor = .systemBlue
+        
+        setStatusButton.layer.shadowOffset = CGSize(width: 4, height: 4)
+        setStatusButton.layer.shadowRadius = 4
+        setStatusButton.layer.shadowColor = UIColor.black.cgColor
+        setStatusButton.layer.shadowOpacity = 0.7
+        setStatusButton.translatesAutoresizingMaskIntoConstraints = false
+        
+        return setStatusButton
+    }()
     
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
+    // MARK: - setup UI objects
     
     private func setupImageView() {
         superview?.addSubview(avatarImageView)
@@ -69,64 +102,80 @@ class ProfileHeaderView: UIView {
         ])
     }
     
-    private func setupNameLabel() {
-        superview?.addSubview(fullNameLabel)
+    private func setupStackView() {
+        superview?.addSubview(stackView)
+        
+        stackView.addArrangedSubview(fullNameLabel)
+        stackView.addArrangedSubview(statusLabel)
+        stackView.addArrangedSubview(statusTextField)
         
         NSLayoutConstraint.activate([
-            fullNameLabel.topAnchor.constraint(equalTo: superview!.safeAreaLayoutGuide.topAnchor, constant: 27),
-            fullNameLabel.leftAnchor.constraint(equalTo: avatarImageView.rightAnchor, constant: 16)
+            stackView.topAnchor.constraint(equalTo: avatarImageView.topAnchor, constant: 0),
+            stackView.leadingAnchor.constraint(equalTo: avatarImageView.trailingAnchor, constant: 16),
+            stackView.trailingAnchor.constraint(equalTo: superview!.safeAreaLayoutGuide.trailingAnchor, constant: -16),
+            stackView.bottomAnchor.constraint(equalTo: avatarImageView.bottomAnchor, constant: 0)
         ])
     }
-    
-    private func setupSomeTextLabel() {
-        superview?.addSubview(statusLabel)
-        
-        NSLayoutConstraint.activate([
-            statusLabel.leftAnchor.constraint(equalTo: avatarImageView.rightAnchor, constant: 16),
-            statusLabel.bottomAnchor.constraint(equalTo: statusButton.topAnchor, constant: -34)
-        ])
-    }
-    
-    
-    
-    
-    let statusButton = UIButton()
     
     private func setupStatusButton() {
-        superview?.addSubview(statusButton)
+        superview?.addSubview(setStatusButton)
         
-        statusButton.setTitle("Show status", for: .normal)
-        statusButton.setTitleColor(.white, for: .normal)
-        statusButton.layer.cornerRadius = 4
-        statusButton.backgroundColor = .systemBlue
+        NSLayoutConstraint.activate([
+            setStatusButton.heightAnchor.constraint(equalToConstant: 50),
+            setStatusButton.leftAnchor.constraint(equalTo: superview!.safeAreaLayoutGuide.leftAnchor, constant: 16),
+            setStatusButton.rightAnchor.constraint(equalTo: superview!.safeAreaLayoutGuide.rightAnchor, constant: -16),
+            setStatusButton.topAnchor.constraint(equalTo: avatarImageView.bottomAnchor, constant: 16)
+        ])
         
-        statusButton.layer.shadowOffset = CGSize(width: 4, height: 4)
-        statusButton.layer.shadowRadius = 4
-        statusButton.layer.shadowColor = UIColor.black.cgColor
-        statusButton.layer.shadowOpacity = 0.7
-        
-        statusButton.translatesAutoresizingMaskIntoConstraints = false
-        let heighConstr = statusButton.heightAnchor.constraint(equalToConstant: 50)
-        let leftConstr = statusButton.leftAnchor.constraint(equalTo: superview!.safeAreaLayoutGuide.leftAnchor, constant: 16)
-        let rightConstr = statusButton.rightAnchor.constraint(equalTo: superview!.safeAreaLayoutGuide.rightAnchor, constant: -16)
-        let topConstr = statusButton.topAnchor.constraint(equalTo: avatarImageView.bottomAnchor, constant: 16)
-        NSLayoutConstraint.activate([heighConstr, leftConstr, rightConstr, topConstr])
-        
-        statusButton.addTarget(self, action: #selector(tap), for: .touchUpInside)
+        setStatusButton.addTarget(self, action: #selector(tap), for: .touchUpInside)
     }
     
     @objc private func tap() {
+        
+        
         print(statusLabel.text ?? "Что-то не сработало")
     }
     
-
     
+//    let statusButton = UIButton()
+    
+//    private func setupStatusButton() {
+//        superview?.addSubview(statusButton)
+//
+//        statusButton.setTitle("Show status", for: .normal)
+//        statusButton.setTitleColor(.white, for: .normal)
+//        statusButton.layer.cornerRadius = 4
+//        statusButton.backgroundColor = .systemBlue
+//
+//        statusButton.layer.shadowOffset = CGSize(width: 4, height: 4)
+//        statusButton.layer.shadowRadius = 4
+//        statusButton.layer.shadowColor = UIColor.black.cgColor
+//        statusButton.layer.shadowOpacity = 0.7
+//
+//        statusButton.translatesAutoresizingMaskIntoConstraints = false
+//        let heighConstr = statusButton.heightAnchor.constraint(equalToConstant: 50)
+//        let leftConstr = statusButton.leftAnchor.constraint(equalTo: superview!.safeAreaLayoutGuide.leftAnchor, constant: 16)
+//        let rightConstr = statusButton.rightAnchor.constraint(equalTo: superview!.safeAreaLayoutGuide.rightAnchor, constant: -16)
+//        let topConstr = statusButton.topAnchor.constraint(equalTo: avatarImageView.bottomAnchor, constant: 16)
+//        NSLayoutConstraint.activate([heighConstr, leftConstr, rightConstr, topConstr])
+//
+//        statusButton.addTarget(self, action: #selector(tap), for: .touchUpInside)
+//    }
+//
+//    @objc private func tap() {
+//        print(statusLabel.text ?? "Что-то не сработало")
+//    }
+    
+
+    // MARK: - apply settings
+
     func setupPHView() {
         
         setupImageView()
-        setupNameLabel()
+//        setupNameLabel()
         setupStatusButton()
-        setupSomeTextLabel()
+//        setupSomeTextLabel()
+        setupStackView()
         
     }
 }
